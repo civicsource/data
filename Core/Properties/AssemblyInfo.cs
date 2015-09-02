5b1afad002e7b2a1e4628d314a20e07fcaf3df9d
Chad Lee <chad.lee@archoninfosys.com>
2015-09-02 13:20:22 -0500

    Adding nuspec file for Data

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Archon Webhooks Core")]
+[assembly: AssemblyDescription("Utility classes to create databases and build schemas from sql scripts")]
