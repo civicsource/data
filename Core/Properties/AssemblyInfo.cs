de32038e460e178c9b28679820d1d668526ce81f
Chad Lee <chad.lee@archoninfosys.com>
2015-09-03 09:40:19 -0500

    Fixing assembly title

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Archon Webhooks Core")]
+[assembly: AssemblyTitle("Archon Data Utilities")]
 [assembly: AssemblyDescription("Utility classes to create databases and build schemas from sql scripts")]
