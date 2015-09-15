67c8cf484da91671fd7a8a19c451bf23180e6bd5
Chad Lee <chad.lee@archoninfosys.com>
2015-09-15 13:03:21 -0500

    Don't include extension method on raw Exception

--- Core/ExceptionTypeExtensions.cs
+++ Core/ExceptionTypeExtensions.cs
@@ -5,11 +5,6 @@ namespace Archon.Data
 {
 	public static class ExceptionTypeExtensions
 	{
-		public static bool IsSqlDuplicateKeyException(this Exception ex)
-		{
-			return IsDuplicateKeyException(ex as SqlException);
-		}
-
 		public static bool IsDuplicateKeyException(this SqlException ex)
 		{
 			if (ex == null)
