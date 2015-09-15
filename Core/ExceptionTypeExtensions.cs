7b7f01fe6b7ded579cbc8ee7e2f1f59d3036aa79
Chad Lee <chad.lee@archoninfosys.com>
2015-09-15 12:57:03 -0500

    Adding extension methods to determine if sql duplicate key exception

---
+++ Core/ExceptionTypeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Archon.Data
+{
+	public static class ExceptionTypeExtensions
+	{
+		public static bool IsSqlDuplicateKeyException(this Exception ex)
+		{
+			return IsDuplicateKeyException(ex as SqlException);
+		}
+
+		public static bool IsDuplicateKeyException(this SqlException ex)
+		{
+			if (ex == null)
+				return false;
+
+			return ex.Number == 2627;
+		}
+	}
+}
