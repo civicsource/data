63a42dd1438bf3362bda3f0d95d63b4997d7b968
Chad Lee <chad.lee@archoninfosys.com>
2015-09-15 11:44:13 -0500

    Adding an EnsureTransaction method

--- Core/ConnectionExtensions.cs
+++ Core/ConnectionExtensions.cs
@@ -9,5 +9,17 @@ namespace Archon.Data
 			if (conn.State != ConnectionState.Open)
 				conn.Open();
 		}
+
+		public static IDbTransaction EnsureTransaction(this IDbConnection conn)
+		{
+			conn.EnsureOpen();
+			return conn.BeginTransaction();
+		}
+
+		public static IDbTransaction EnsureTransaction(this IDbConnection conn, IsolationLevel isolationLevel)
+		{
+			conn.EnsureOpen();
+			return conn.BeginTransaction(isolationLevel);
+		}
 	}
 }
